a049407b1a97b555273c58b9c9d7a767e773376e
Brad Laue <brad@brad-x.com>
2019-10-12 18:19:59 -0400

    Install eselect-repository to begin with, eliminate steps later

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,8 +5,8 @@ set -e
 echo "Syncing initial portage tree... "
 emerge-webrsync
 
-echo "Installing git... "
-USE="-perl -pcre -pcre-jit" emerge dev-vcs/git -v
+echo "Installing git and eselect-repository... "
+USE="-perl -pcre -pcre-jit" emerge dev-vcs/git eselect-repository -v
 
 echo "Deploying our own portage directory configurations... "
 rm -rf /etc/portage/
@@ -18,8 +18,6 @@ cd /etc/portage/repos.conf/
 ## Override the gentoo portage tree - use ours as it's in sync with the BINHOSTS (theoretically)
 curl -LO http://packages.brad-x.com/brad-x.conf
 cd
-eselect profile set default/linux/amd64/17.1
-emerge eselect-repository -v
 eselect repository list
 eselect repository add brad-x-overlay git git://github.com/brad-x/brad-x-overlay.git
 emerge --sync
